7ed59bde909e5e63528afefad956d7ca58f3029f
keshew <artyom.korotckov2017@yandex.ru>
2022-07-27 13:02:28 +0500

    initial

---
+++ PlaceToEat/MainViewController.swift
@@ -0,0 +1,48 @@
+//
+//  MainViewController.swift
+//  PlaceToEat
+//
+//  Created by Артём Коротков on 27.07.2022.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+
+    var cafeTest = ["TramCafe","yChurki"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+  
+    }
+
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return cafeTest.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.imageView?.image = UIImage(named: cafeTest[indexPath.row])
+        cell.textLabel?.text = cafeTest[indexPath.row]
+
+        return cell
+    }
+    
+
+     /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
