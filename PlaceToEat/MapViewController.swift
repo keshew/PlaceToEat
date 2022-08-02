71a19bf5381b1c6bc3a20cda8e5633664a6b3656
keshew <artyom.korotckov2017@yandex.ru>
2022-08-02 18:25:15 +0500

    work with map

--- PlaceToEat/MapViewController.swift
+++ PlaceToEat/MapViewController.swift
@@ -6,12 +6,50 @@
 //
 
 import UIKit
-
+import MapKit
 class MapViewController: UIViewController {
 
+    var place: Place!
+    
+    
+    
+    @IBOutlet var mapView: MKMapView!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupPlacemark()
     }
+    
+    
+    private func setupPlacemark() {
+        guard let location = place.location else { return }
+        
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(location) { placemarks, error in
+            
+            if let error = error {
+                print(error)
+                return
+            }
+            guard let placemarks = placemarks else { return }
+            
+            let placemark = placemarks.first
+            
+            let annotation = MKPointAnnotation()
+            annotation.title = self.place.name
+            annotation.subtitle = self.place.type
+            
+            guard let placemarkLocation = placemark?.location else { return }
+            
+            annotation.coordinate = placemarkLocation.coordinate
+            
+            self.mapView.showAnnotations([annotation], animated: true)
+            self.mapView.selectAnnotation(annotation, animated: true)
+        }
+    }
+    
+    
     
     
     
@@ -19,5 +57,6 @@ class MapViewController: UIViewController {
     @IBAction func closeVC() {
         dismiss(animated: true)
     }
+
     
 }
