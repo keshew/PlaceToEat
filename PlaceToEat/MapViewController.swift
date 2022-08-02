d3fd8c5de3e797b834a994e1fbd52dc037cd827c
keshew <artyom.korotckov2017@yandex.ru>
2022-08-02 17:49:46 +0500

    add map

---
+++ PlaceToEat/MapViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MapViewController.swift
+//  PlaceToEat
+//
+//  Created by Артём Коротков on 02.08.2022.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+    
+    @IBAction func closeVC() {
+        dismiss(animated: true)
+    }
+    
+}
