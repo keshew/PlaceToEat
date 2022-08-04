4f233ee5e874345d051393c5ec3bd8ccd6534f98
keshew <artyom.korotckov2017@yandex.ru>
2022-08-04 16:12:57 +0500

    get adress on label

--- PlaceToEat/MapViewController.swift
+++ PlaceToEat/MapViewController.swift
@@ -24,6 +24,8 @@ class MapViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        adressLabel.text = ""
         mapView.delegate = self
         setupMapView()
         checkLocationServices()
@@ -108,6 +110,19 @@ class MapViewController: UIViewController {
         self.present(alert, animated: true)
     }
     
+    
+    private func getCenterLocation(for mapView: MKMapView) -> CLLocation {
+        
+        let latitude = mapView.centerCoordinate.latitude
+        let longitude = mapView.centerCoordinate.longitude
+        
+        return CLLocation(latitude: latitude, longitude: longitude)
+        
+    }
+    
+    
+    
+    
     private func showUserLocation() {
         
         if let location = locationManager.location?.coordinate {
@@ -169,6 +184,41 @@ extension MapViewController: MKMapViewDelegate {
         
         return annotationView
     }
+    
+    
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        
+        let center = getCenterLocation(for: mapView)
+        let geocoder = CLGeocoder()
+        
+        geocoder.reverseGeocodeLocation(center) { (placemarks, error) in
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            guard let placemarks = placemarks else { return }
+            
+            let placemark = placemarks.first
+            let streetName = placemark?.thoroughfare
+            let buildNumber = placemark?.subThoroughfare
+            
+            DispatchQueue.main.async {
+            if streetName != nil && buildNumber != nil {
+                self.adressLabel.text = "\(streetName!), \(buildNumber!)"
+            } else if streetName != nil {
+                self.adressLabel.text = "\(streetName!)"
+            } else {
+                self.adressLabel.text = ""
+                }
+            }
+        }
+    }
+    
+    
+    
+    
+    
 }
 
 
